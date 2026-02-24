2254b0f92aac0cbede641c6d6b3e05d48a6fbb9f
Thomas Matthew Reed <thomasmatthewreed@MacBook-Air-2.local>
2026-02-24 14:47:13 +1300

    Initial commit for part3 backend

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 
 app.use(cors())
 
+app.use(express.static('dist'))
+
 let notes = [
   {
     id: '1',
@@ -33,9 +35,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
@@ -91,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
