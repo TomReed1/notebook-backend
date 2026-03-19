2d3016bdd13391181f826d9c79f9d3fba0ae8018
Thomas Matthew Reed <thomasmatthewreed@MacBook-Air-2.local>
2026-03-19 13:15:01 +1300

    notes backend

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://thomasmatthewreed_db_user:${password}@cluster0.dgdp22l.mongodb.net/noteApp?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+//const note = new Note({
+// content: 'HTML is easy',
+ // important: true,
+// })
+
+// note.save().then(result => {
+ // console.log('note saved!')
+ // mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
